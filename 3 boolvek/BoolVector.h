b2cd2c10e28325556587fac1e9df995e350b89d5
1M04L <mikhailulianenko@gmail.com>
2024-11-07 13:37:23 +0700

    Update BoolVector.h

--- 3 boolvek/BoolVector.h
+++ 3 boolvek/BoolVector.h
@@ -1,27 +1,43 @@
 #pragma once
+#include <iostream>
+#include <cstring>
 
 class BoolVector
 {
 public:
     class Rank;
-    
+
 public:
     using Cell = unsigned char;
     static const int CellSize = 8; // sizeof(Cell) * 8
     BoolVector(int length = CellSize);
-    BoolVector(int length, bool value); // TODO: implement
+    BoolVector(int length, bool value);
+    BoolVector(const char* str); // Конструктор из массива const char *
     BoolVector(const BoolVector& other);
     ~BoolVector();
-    
+
     int length() const;
-    
-    void swap(BoolVector& other); 
+
+    void swap(BoolVector& other);
+    void invertAll();
     bool bitValue(int index) const;
+    void setAllToZero();
     void setBitValue(int index, bool value);
-    
+
     BoolVector& operator=(const BoolVector& other);
-    Rank operator[](int index);
-    
+    Rank operator[](int index) const; // Получение компоненты (только чтение)
+
+    BoolVector operator&(const BoolVector& other) const;
+    BoolVector& operator&=(const BoolVector& other);
+    BoolVector operator|(const BoolVector& other) const;
+    BoolVector& operator|=(const BoolVector& other);
+    BoolVector operator^(const BoolVector& other) const;
+    BoolVector operator~() const;
+
+    // Потоковый ввод/вывод
+    friend std::ostream& operator<<(std::ostream& out, const BoolVector& bv);
+    friend std::istream& operator>>(std::istream& in, BoolVector& bv);
+
 private:
     int _excessRankCount() const;
     static Cell _mask(int index);
@@ -35,13 +51,18 @@ private:
 class BoolVector::Rank
 {
 public:
-    Rank() = default;
-    Rank(Cell* cell, Cell mask);
-    Rank& operator=(const Rank& other);
-    Rank& operator=(bool value);
-    operator bool() const;
-    
+   
+    Rank(const Cell* cell, Cell mask); 
+
+    Rank& operator&=(const Rank& other);
+    Rank& operator|=(const Rank& other);
+    Rank& operator^=(const Rank& other);
+    Rank operator~() const;
+
+    // Преобразование в bool
+    operator bool() const; 
+
 private:
-    Cell* m_cell = nullptr;
+    const Cell* m_cell = nullptr;
     Cell m_mask = 0;
 };
